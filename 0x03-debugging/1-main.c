8dee4890b74bce784008503a412f4d18127d7870
Dekunle <adekunlesa10@yahoo.com>
2022-07-07 04:30:33 -0700

    leap year

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-/* don’t delete it */
+/*
 	while (i < 10)
 	{
 		putchar(i);
 	}
-
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
