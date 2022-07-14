c0fce3dd7e1bee1c0fcae262d9169e273b9f6c1a
DekunleJr <106774760+DekunleJr@users.noreply.github.com>
2022-07-14 16:15:16 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n1 = -n;
-		putchar('-');
+		_putchar('-');
 	} else
 	{
 		n1 = n;
@@ -20,5 +20,5 @@ void print_number(int n)
 	if (n1 / 10)
 		print_number(n1 / 10);
 
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
