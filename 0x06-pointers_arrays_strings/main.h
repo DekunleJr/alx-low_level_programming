52e63c24b2045093bb17fecade68b83d0d78e56a
Dekunle <adekunlesa10@yahoo.com>
2022-07-14 20:52:11 -0700

    Numbers have life

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void print_line(char *c, int s, int l);
+int _putchar(char c);
 
 
 #endif 
