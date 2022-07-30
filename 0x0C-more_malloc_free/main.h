7f7807a35997b24e7726d1bdfb6996adb29b8c29
Dekunle <adekunlesa10@yahoo.com>
2022-07-30 15:01:42 -0700

    task 4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void simple_print_buffer(char *buffer, unsigned int size);
-int main(void);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 void errors(void);
